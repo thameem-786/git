04e671166ba8f581caeb3c8962e92a139730d483
thameem-786 <thameemmohammed420@gmail.com>
2026-06-08 20:27:00 +0530

    version1.1

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner mt= new Scanner(System.in);
+        int a=mt.nextInt();
+        int b=mt.nextInt();
+        int c=a+b;
+        int d=a-b;
+        System.out.println("Addition: " + c);
+        System.out.println("Subtraction: " + d);
+    }
+}
