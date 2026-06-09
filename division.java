15724f71f7fceac0c01c3b2fa0e05e85fe84b4b1
thameem-786 <thameemmohammed420@gmail.com>
2026-06-09 09:17:18 +0530

    newfunction:division

---
+++ division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class division{
+    public static void main(String args[]) {
+        Scanner mt=new Scanner (System.in);
+        int a=mt.nextInt();
+        int b=mt.nextInt();
+        int c=a/b;
+        System.out.println("Division: "+c);
+    }
+}
